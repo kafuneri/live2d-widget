82b2598e8d071bd6b585f569fd0ba84a58bac8d1
Sakura <69903799+kafuneri@users.noreply.github.com>
2024-10-09 14:10:32 +0800

    Update autoload.js

--- autoload.js
+++ autoload.js
@@ -1,6 +1,20 @@
-// live2d_path 参数建议使用绝对路径
-const live2d_path = "https://fastly.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/";
-//const live2d_path = "/live2d-widget/";
+// Matomo
+//var _paq = _paq || [];
+//_paq.push(['trackPageView']);
+//_paq.push(['enableLinkTracking']);
+//(function() {
+//	var u = 'https://zsq.im/matomo/';
+//	_paq.push(['setTrackerUrl', u + 'matomo.php']);
+//	_paq.push(['setSiteId', '2']);
+//	var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
+//	g.type = 'text/javascript'; g.async = true; g.defer = true; g.src = u + 'matomo.js'; s.parentNode.insertBefore(g,s);
+//})();
+// End Matomo Code
+
+// Live2d
+// 注意：live2d_path 参数应使用绝对路径
+const live2d_path = "https://fastly.jsdelivr.net/gh/kafuneri/live2d-widget@latest/";
+//const live2d_path = "/lib/waifu/";
 
 // 封装异步加载资源的方法
 function loadExternalResource(url, type) {
@@ -31,15 +45,16 @@ if (screen.width >= 768) {
 		loadExternalResource(live2d_path + "live2d.min.js", "js"),
 		loadExternalResource(live2d_path + "waifu-tips.js", "js")
 	]).then(() => {
-		// 配置选项的具体用法见 README.md
 		initWidget({
-			waifuPath: live2d_path + "waifu-tips.json",
-			//apiPath: "https://live2d.fghrsh.net/api/",
-			cdnPath: "https://fastly.jsdelivr.net/gh/fghrsh/live2d_api/",
-			tools: ["hitokoto", "asteroids", "switch-model", "switch-texture", "photo", "info", "quit"]
+			waifuPath: "/lib/waifu-tips.json",
+			apiPath: "https://api.zsq.im/live2d", // https://live2d.fghrsh.net/api
+			//cdnPath: "https://cdn.jsdelivr.net/gh/fghrsh/live2d_api/"
 		});
 	});
 }
+// initWidget 第一个参数为 waifu-tips.json 的路径，第二个参数为 API 地址
+// API 后端可自行搭建，参考 https://github.com/fghrsh/live2d_api
+// 初始化看板娘会自动加载指定目录下的 waifu-tips.json
 
 console.log(`
   く__,.ヘヽ.        /  ,ー､ 〉
@@ -60,3 +75,4 @@ console.log(`
                   ﾄ-,/  |___./
                   'ｰ'    !_,.:
 `);
+// End Live2d Code
