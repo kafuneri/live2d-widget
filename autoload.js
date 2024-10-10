dc99889bf71237742597980364923b4ca47e03da
Sakura <69903799+kafuneri@users.noreply.github.com>
2024-10-10 21:54:16 +0800

    Update autoload.js

--- autoload.js
+++ autoload.js
@@ -13,7 +13,7 @@
 
 // Live2d
 // 注意：live2d_path 参数应使用绝对路径
-const live2d_path = "https://fastly.jsdelivr.net/gh/kafuneri/live2d-widget@0.1.2/";
+const live2d_path = "https://fastly.jsdelivr.net/gh/kafuneri/live2d-widget@latest/";
 //const live2d_path = "/lib/waifu/";
 
 // 封装异步加载资源的方法
